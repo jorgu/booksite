eab0ad1b423ac1451555de349ceb425dbbf34cd5
jorgu <jorgu@users.noreply.github.com>
2025-09-14 11:01:10 +0200

    refaktorisering

--- booksite.js
+++ booksite.js
@@ -12,7 +12,7 @@ init();
 let writers = getAllAuthors(books);
 let id = 0;
 writers.forEach(writer => {
-    skapaTabell(++id, writer);
+    skapaTabell(++id, writer, isAdmin);
     allTables.push('tabell' + id);
 });
 
@@ -33,7 +33,7 @@ function visaTabell(tabellId) {
 }
 
 // Fyll tabellerna 
-function skapaTabell(Id, writer) {
+function skapaTabell(Id, writer, isAdmin) {
     const tableId           = 'tabell' + Id;
     const tableContainer    = document.getElementById('tableContainer');
     const newTable          = document.createElement('table');
@@ -69,7 +69,7 @@ function skapaTabell(Id, writer) {
 
     bookList.forEach( book => {
         bookSerie == book.serie ? color : color = notDefaultColor;
-        skapaRad(color, newTable, book)
+        skapaRad(color, newTable, book, isAdmin)
         bookSerie = book.serie;    
     } )
 
@@ -83,12 +83,12 @@ function skapaTabell(Id, writer) {
         
 }
 
-function skapaCell(color, text, isAdmin = false) {
+function skapaCell(color, text, bought, isAdmin = false) {
     const cell = document.createElement('td');
     if (color.length > 0) { cell.style.color = color}
     cell.textContent = text;
 
-    if (isAdmin) {
+    if (isAdmin && bought == 'TRUE') {
         cell.style.color = 'lightgray'; // Gör texten grå
         cell.style.fontStyle = 'italic'
         cell.style.textDecoration = 'line-through'; // Gör texten överstruken
@@ -98,17 +98,13 @@ function skapaCell(color, text, isAdmin = false) {
     return cell;
 }
 
-function skapaRad(color, tabell, book) {
+function skapaRad(color, tabell, book, isAdmin) {
     const rad = document.createElement('tr');
-    let isBought = false;
-    if (isAdmin) {
-        book.bought.toLowerCase() == "true" ? isBought = true : isBought = false;
-    }
-
+    
     // Exempel: Kolumn 1 vanlig, Kolumn 2 grå, Kolumn 3 grå + överstruken
     rad.appendChild(skapaCell(color, book.serie));
-    rad.appendChild(skapaCell(color, book.book, book.bought)); // grå
-    rad.appendChild(skapaCell(color, book.year, book.bought)); // grå + överstruken
+    rad.appendChild(skapaCell(color, book.book, book.bought, isAdmin)); // grå
+    rad.appendChild(skapaCell(color, book.year, book.bought, isAdmin)); // grå + överstruken
 
     tabell.appendChild(rad);
 }
@@ -129,3 +125,74 @@ function getAllAuthors (bookList) {
 
 
 
+function init() {
+    books.push({author: 'Liza Marklund', serie: 'Annika Bengtzon', book: 'Sprängaren', year: 1998, bought: 'TRUE'});
+	books.push({author: 'Liza Marklund', serie: 'Annika Bengtzon', book: 'Studio sex', year: 1999, bought: 'TRUE'});
+	books.push({author: 'Liza Marklund', serie: 'Annika Bengtzon', book: 'Paradiset', year: 2000, bought: 'TRUE'});
+	books.push({author: 'Liza Marklund', serie: 'Annika Bengtzon', book: 'Prime time', year: 2002, bought: 'TRUE'});
+	books.push({author: 'Liza Marklund', serie: 'Annika Bengtzon', book: 'Den röda vargen', year: 2003, bought: 'TRUE'});
+	books.push({author: 'Liza Marklund', serie: 'Annika Bengtzon', book: 'Nobels testamente', year: 2006, bought: 'TRUE'});
+	books.push({author: 'Liza Marklund', serie: 'Annika Bengtzon', book: 'Livstid', year: 2007, bought: 'TRUE'});
+	books.push({author: 'Liza Marklund', serie: 'Annika Bengtzon', book: 'En plats i solen', year: 2008, bought: 'TRUE'});
+	books.push({author: 'Liza Marklund', serie: 'Annika Bengtzon', book: 'Du gamla, du fria', year: 2011, bought: 'FALSE'});
+	books.push({author: 'Liza Marklund', serie: 'Annika Bengtzon', book: 'Lyckliga gatan', year: 2013, bought: 'FALSE'});
+	books.push({author: 'Liza Marklund', serie: 'Annika Bengtzon', book: 'Järnblod', year: 2015, bought: 'FALSE'});
+	books.push({author: 'Liza Marklund', serie: 'Stenträsk-triologin', book: 'Polcirkeln', year: 2021, bought: 'FALSE'});
+	books.push({author: 'Liza Marklund', serie: 'Stenträsk-triologin', book: 'Kallmyren', year: 2022, bought: 'FALSE'});
+	books.push({author: 'Liza Marklund', serie: 'Stenträsk-triologin', book: 'Stormberget', year: 2023, bought: 'FALSE'});
+	books.push({author: 'Mari Jungstedt', serie: 'Anders Knutas', book: 'Den du inte ser', year: 2003, bought: 'FALSE'});
+	books.push({author: 'Mari Jungstedt', serie: 'Anders Knutas', book: 'I denna stilla natt', year: 2004, bought: 'TRUE'});
+	books.push({author: 'Mari Jungstedt', serie: 'Anders Knutas', book: 'Den inre kretsen', year: 2005, bought: 'TRUE'});
+	books.push({author: 'Mari Jungstedt', serie: 'Anders Knutas', book: 'Den döende dandyn', year: 2006, bought: 'FALSE'});
+	books.push({author: 'Mari Jungstedt', serie: 'Anders Knutas', book: 'I denna ljuva sommartid', year: 2007, bought: 'TRUE'});
+	books.push({author: 'Mari Jungstedt', serie: 'Anders Knutas', book: 'Den mörka ängeln', year: 2008, bought: 'TRUE'});
+	books.push({author: 'Mari Jungstedt', serie: 'Anders Knutas', book: 'Den dubbla tystnaden', year: 2009, bought: 'TRUE'});
+	books.push({author: 'Mari Jungstedt', serie: 'Anders Knutas', book: 'Den farliga leken', year: 2010, bought: 'FALSE'});
+	books.push({author: 'Mari Jungstedt', serie: 'Anders Knutas', book: 'Det fjärde offret', year: 2011, bought: 'TRUE'});
+	books.push({author: 'Mari Jungstedt', serie: 'Anders Knutas', book: 'Den sista akten', year: 2012, bought: 'FALSE'});
+	books.push({author: 'Mari Jungstedt', serie: 'Anders Knutas', book: 'Du går inte ensam', year: 2013, bought: 'FALSE'});
+	books.push({author: 'Mari Jungstedt', serie: 'Anders Knutas', book: 'Den man älskar', year: 2014, bought: 'FALSE'});
+	books.push({author: 'Mari Jungstedt', serie: 'Anders Knutas', book: 'Det andra ansiktet', year: 2016, bought: 'FALSE'});
+	books.push({author: 'Mari Jungstedt', serie: 'Anders Knutas', book: 'Ett mörker mitt ibland oss', year: 2018, bought: 'FALSE'});
+	books.push({author: 'Mari Jungstedt', serie: 'Anders Knutas', book: 'Jag ser dig', year: 2019, bought: 'FALSE'});
+	books.push({author: 'Mari Jungstedt', serie: 'Anders Knutas', book: 'Där den sista lampan lyser', year: 2021, bought: 'FALSE'});
+	books.push({author: 'Mari Jungstedt', serie: 'Anders Knutas', book: 'Det slutna rummet', year: 2023, bought: 'FALSE'});
+	books.push({author: 'Mari Jungstedt', serie: 'Anders Knutas', book: 'Den sista utposten', year: 2024, bought: 'FALSE'});
+	books.push({author: 'Åsa Larsson', serie: 'Rebecka Martinsson', book: 'Solstorm', year: 2003, bought: 'TRUE'});
+	books.push({author: 'Åsa Larsson', serie: 'Rebecka Martinsson', book: 'Det blod som spillts', year: 2004, bought: 'TRUE'});
+	books.push({author: 'Åsa Larsson', serie: 'Rebecka Martinsson', book: 'Svart stig', year: 2006, bought: 'FALSE'});
+	books.push({author: 'Åsa Larsson', serie: 'Rebecka Martinsson', book: 'Till dess din vrede upphör', year: 2008, bought: 'FALSE'});
+	books.push({author: 'Åsa Larsson', serie: 'Rebecka Martinsson', book: 'Till offer åt Molok', year: 2012, bought: 'FALSE'});
+	books.push({author: 'Åsa Larsson', serie: 'Rebecka Martinsson', book: 'Fädernas missgärningar', year: 2021, bought: 'FALSE'});
+	books.push({author: 'Henning Mankell', serie: 'Kurt Wallander', book: 'Mördare utan ansikte', year: 1991, bought: 'TRUE'});
+	books.push({author: 'Henning Mankell', serie: 'Kurt Wallander', book: 'Hundarna i Riga', year: 1992, bought: 'TRUE'});
+	books.push({author: 'Henning Mankell', serie: 'Kurt Wallander', book: 'Den vita lejoninnan', year: 1993, bought: 'TRUE'});
+	books.push({author: 'Henning Mankell', serie: 'Kurt Wallander', book: 'Mannen som log', year: 1994, bought: 'TRUE'});
+	books.push({author: 'Henning Mankell', serie: 'Kurt Wallander', book: 'Villospår', year: 1995, bought: 'TRUE'});
+	books.push({author: 'Henning Mankell', serie: 'Kurt Wallander', book: 'Den femte kvinnan', year: 1996, bought: 'TRUE'});
+	books.push({author: 'Henning Mankell', serie: 'Kurt Wallander', book: 'Steget efter', year: 1997, bought: 'TRUE'});
+	books.push({author: 'Henning Mankell', serie: 'Kurt Wallander', book: 'Brandvägg', year: 1998, bought: 'TRUE'});
+	books.push({author: 'Henning Mankell', serie: 'Kurt Wallander', book: 'Pyramiden', year: 1999, bought: 'TRUE'});
+	books.push({author: 'Henning Mankell', serie: 'Kurt Wallander', book: 'Innan frosten', year: 2002, bought: 'TRUE'});
+	books.push({author: 'Henning Mankell', serie: 'Kurt Wallander', book: 'Den orolige mannen', year: 2009, bought: 'TRUE'});
+	books.push({author: 'Henning Mankell', serie: 'Kurt Wallander', book: 'Handen', year: 2013, bought: 'FALSE'});
+	books.push({author: 'Hjorth Rosenfeldt', serie: 'Sebastian Bergman', book: 'Det fördolda', year: 2010, bought: 'FALSE'});
+	books.push({author: 'Hjorth Rosenfeldt', serie: 'Sebastian Bergman', book: 'Lärjungen', year: 2011, bought: 'FALSE'});
+	books.push({author: 'Hjorth Rosenfeldt', serie: 'Sebastian Bergman', book: 'Fjällgraven', year: 2012, bought: 'FALSE'});
+	books.push({author: 'Hjorth Rosenfeldt', serie: 'Sebastian Bergman', book: 'Den stumma flickan', year: 2014, bought: 'FALSE'});
+	books.push({author: 'Hjorth Rosenfeldt', serie: 'Sebastian Bergman', book: 'De underkända', year: 2015, bought: 'FALSE'});
+	books.push({author: 'Hjorth Rosenfeldt', serie: 'Sebastian Bergman', book: 'En högre rättvisa', year: 2018, bought: 'FALSE'});
+	books.push({author: 'Hjorth Rosenfeldt', serie: 'Sebastian Bergman', book: 'Som man sår', year: 2021, bought: 'FALSE'});
+	books.push({author: 'Hjorth Rosenfeldt', serie: 'Sebastian Bergman', book: 'Skulden man bär', year: 2023, bought: 'FALSE'});
+	books.push({author: 'Camilla Läckberg', serie: 'Fjällbacka', book: 'Isprinsessan', year: 2003, bought: 'FALSE'});
+	books.push({author: 'Camilla Läckberg', serie: 'Fjällbacka', book: 'Predikanten', year: 2004, bought: 'FALSE'});
+	books.push({author: 'Camilla Läckberg', serie: 'Fjällbacka', book: 'Stenhuggaren', year: 2005, bought: 'FALSE'});
+	books.push({author: 'Camilla Läckberg', serie: 'Fjällbacka', book: 'Olycksfågeln', year: 2006, bought: 'FALSE'});
+	books.push({author: 'Camilla Läckberg', serie: 'Fjällbacka', book: 'Tyskungen', year: 2007, bought: 'FALSE'});
+	books.push({author: 'Camilla Läckberg', serie: 'Fjällbacka', book: 'Sjöjungfrun', year: 2008, bought: 'FALSE'});
+	books.push({author: 'Camilla Läckberg', serie: 'Fjällbacka', book: 'Fyrvaktaren', year: 2009, bought: 'FALSE'});
+	books.push({author: 'Camilla Läckberg', serie: 'Fjällbacka', book: 'Änglamakerskan', year: 2011, bought: 'FALSE'});
+	books.push({author: 'Camilla Läckberg', serie: 'Fjällbacka', book: 'Lejontämjaren', year: 2014, bought: 'FALSE'});
+	books.push({author: 'Camilla Läckberg', serie: 'Fjällbacka', book: 'Häxan', year: 2017, bought: 'FALSE'});
+	books.push({author: 'Camilla Läckberg', serie: 'Fjällbacka', book: 'Gökungen', year: 2022, bought: 'FALSE'});
+}
