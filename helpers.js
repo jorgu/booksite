4bdb4c7075a9f7d5ab8c29848ec9395d830f2ba6
jorgu <jorgu@users.noreply.github.com>
2025-11-08 13:33:41 +0100

    t

--- helpers.js
+++ helpers.js
@@ -1,5 +1,5 @@
 'use strict'
-import { visaTabell } from './booksite.js'
+//import { visaTabell } from './booksite.js';
 
 // Fyll tabellerna 
 export function skapaTabell(Id, books, writer, isAdmin) {
